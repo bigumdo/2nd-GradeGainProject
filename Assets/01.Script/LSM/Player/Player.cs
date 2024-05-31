e092fc5362490325abfddf1091b2a1bc70c6fff9
bigumdo <showmethe0610@gmail.com>
2024-05-31 22:21:45 +0900

    PlayerSound

---
+++ Assets/01.Script/LSM/Player/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [HideInInspector] public Animator animator;
+    [HideInInspector] public AudioSource audioSource;
+
+    private void Awake()
+    {
+        animator = transform.Find("Visual").GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+
+}
