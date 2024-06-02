87cb1a30d95ca057cc004d946f4a0ed5023b610f
bigumdo <showmethe0610@gmail.com>
2024-06-02 23:46:28 +0900

    HammerSound2

--- Assets/01.Script/LSM/ClickSO.cs
+++ Assets/01.Script/LSM/ClickSO.cs
@@ -13,3 +13,4 @@ public class ClickSO : ScriptableObject
     public Sprite hammer;
 
 }
+
--- Assets/01.Script/LSM/MouseClik.cs
+++ Assets/01.Script/LSM/MouseClik.cs
@@ -33,8 +33,8 @@ public class MouseClik : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && _time <= 0)
         {
             Debug.Log(_clickMoney);
-            _player.animator.SetTrigger("Click");
             _player.audioSource.Play();
+            _player.animator.SetTrigger("Click");
             _time = _clickCoolTime;
         }
         else if (_time >= 0)
