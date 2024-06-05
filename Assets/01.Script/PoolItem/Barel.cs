a8e6f260b2031294b7452791e035cc5c58ba728a
LEE SANG GYU <tkdrb062@gmail.com>
2024-06-05 12:37:51 +0900

    feat: Baral HP

--- Assets/01.Script/PoolItem/Barel.cs
+++ Assets/01.Script/PoolItem/Barel.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 
 public class Barel : PoolableMono
 {
-    private int hp;
-    public void Break()
+    [SerializeField] private int hp;
+    public bool Break()
     {
+        hp -= 1;
+        if (hp > 0) return false;
         gameObject.SetActive(false);
         PoolingManager.Instance.Push(this);
+        return true;
     }
     
     public override void ResetItem()
     {
+        hp = 10;
         transform.position = PoolingManager.Instance.transform.position;
         transform.localRotation = Quaternion.identity;
     }
