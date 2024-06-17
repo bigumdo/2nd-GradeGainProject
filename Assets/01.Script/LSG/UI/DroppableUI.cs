a6c56a72e26b561e41e0971ce42e61d4cd66199f
LEE SANG GYU <tkdrb062@gmail.com>
2024-06-17 12:25:26 +0900

    feat: Drag and Drop

---
+++ Assets/01.Script/LSG/UI/DroppableUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DroppableUI : MonoBehaviour, IPointerEnterHandler, IDropHandler, IPointerExitHandler
+{
+    [SerializeField] private Color _hoverColor;
+    private Image _image;
+    private RectTransform _rect;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+        _rect = transform as RectTransform;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _image.color = _hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _image.color = Color.white;
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.transform.SetParent(transform);
+            eventData.pointerDrag.GetComponent<RectTransform>().position = _rect.position;
+        }
+    }
+
+}
