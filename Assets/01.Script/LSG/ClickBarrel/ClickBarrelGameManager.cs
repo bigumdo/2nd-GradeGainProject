8354ac6ae0fc9245a0af774dbe3d3461a0716968
LEE SANG GYU <tkdrb062@gmail.com>
2024-06-21 08:27:44 +0900

    feat: Title

--- Assets/01.Script/LSG/ClickBarrel/ClickBarrelGameManager.cs
+++ Assets/01.Script/LSG/ClickBarrel/ClickBarrelGameManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ClickBarrelGameManager : MonoSingleton<ClickBarrelGameManager>
 {
+    [SerializeField] private Image _endGamePanel;
+    [SerializeField] private TextMeshProUGUI _endGameTimeText;
+    [SerializeField] private Button _endGameButton;
     [SerializeField] private TextMeshProUGUI _timeText;
     [SerializeField] private TextMeshProUGUI _countText;
     [SerializeField] private List<int> _barrelCountList = new List<int>();
@@ -12,10 +17,18 @@ public class ClickBarrelGameManager : MonoSingleton<ClickBarrelGameManager>
     private float _sec;
     private float _msec;
     private bool _isEndHit;
-
+    private bool _isEndGame;
     public int Count { get; set;}
+    
     private void Update()
     {
+        if (_isEndGame) return;
+        if (Count >= 100)
+        {
+            _endGamePanel.gameObject.SetActive(true);
+            EndGame();
+            _isEndGame = true;
+        }
         TimeRender();
         CountRender();
     }
@@ -32,5 +45,23 @@ public class ClickBarrelGameManager : MonoSingleton<ClickBarrelGameManager>
     private void CountRender()
     {
         _countText.text = $"{Count}";
+    }
+    
+    public void EndGame()
+    {
+        _endGameTimeText.text = $"Time: {_min:00}:{_sec:00}:{_msec:00}";
+        _timeText.text = "00:00:00";
+        _countText.text = "0";
+        _endGameButton.onClick.AddListener(() =>
+        {
+            _endGamePanel.gameObject.SetActive(false);
+            _isEndGame = false;
+            _isEndHit = false;
+            _min = 0;
+            _sec = 0;
+            _msec = 0;
+            Count = 0;
+            SceneManager.LoadScene("LSM");
+        });
     }
 }
